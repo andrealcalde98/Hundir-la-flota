450977fb3b93a894de2340f74153c30d8495c0dc
Raul Barrero <rbarrero1998@gmail.com>
2019-02-22 12:24:45 +0100

    Tablero actualizado.

--- HundirFloa/src/vista/HundirFloa.java
+++ HundirFloa/src/vista/HundirFloa.java
@@ -12,7 +12,7 @@ public class HundirFloa {
     public static void main(String[] args) {
         Tablero tab = new Tablero(9);
         tab.tableroVAcio();
-        System.out.println("Tamaño del tablero: " + tab.tamanyo.length);
+        System.out.println("Tamaño del tablero de " + tab.tamanyo.length+" por "+tab.tamanyo.length);
         tab.mostrarTablero();
         consola.llenarTablero(tab.tamanyo, 9);
     }
