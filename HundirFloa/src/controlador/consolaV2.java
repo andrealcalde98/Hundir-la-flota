ea78ef4685c10395e1d14ec40d062c2a29e1ca03
RKJD <44412937+RKJD@users.noreply.github.com>
2019-03-04 22:45:02 +0100

    NO VA JEJE

--- HundirFloa/src/controlador/consolaV2.java
+++ HundirFloa/src/controlador/consolaV2.java
@@ -212,18 +212,18 @@ public class consolaV2 {
     }
 
     public void bloqueaAdyacentes(int ColumnaIni, int ColumnaFin, int FilaIni, int FilaFin) {
-        while(jugador.tablero.tamanyo[FilaIni - 1][ColumnaIni]>0){
-        jugador.tablero.tamanyo[FilaIni - 1][ColumnaIni] = 2;
-        jugador.tablero.tamanyo[FilaIni - 1][ColumnaIni - 1] = 2;
-        jugador.tablero.tamanyo[FilaIni - 1][ColumnaIni + 1] = 2;
-        jugador.tablero.tamanyo[FilaFin + 1][ColumnaIni] = 2;
-        jugador.tablero.tamanyo[FilaFin + 1][ColumnaIni - 1] = 2;
-        jugador.tablero.tamanyo[FilaFin + 1][ColumnaIni + 1] = 2;
+        if(jugador.tablero.tamanyo[FilaIni - 1][ColumnaIni]>=0){jugador.tablero.tamanyo[FilaIni - 1][ColumnaIni] = 2;};
+        if(jugador.tablero.tamanyo[FilaIni - 1][ColumnaIni - 1]>=0){jugador.tablero.tamanyo[FilaIni - 1][ColumnaIni - 1] = 2;};
+        if(jugador.tablero.tamanyo[FilaIni - 1][ColumnaIni + 1]>=0){jugador.tablero.tamanyo[FilaIni - 1][ColumnaIni + 1] = 2;};
+        if(jugador.tablero.tamanyo[FilaFin + 1][ColumnaIni]>=0){jugador.tablero.tamanyo[FilaFin + 1][ColumnaIni] = 2;};
+        if(jugador.tablero.tamanyo[FilaFin + 1][ColumnaIni - 1]>=0){jugador.tablero.tamanyo[FilaFin + 1][ColumnaIni - 1] = 2;};
+        if(jugador.tablero.tamanyo[FilaFin + 1][ColumnaIni + 1]>=0){jugador.tablero.tamanyo[FilaFin + 1][ColumnaIni + 1] = 2;};
+        
         for (int x = FilaIni; x <= FilaFin; x++) {
-
-            jugador.tablero.tamanyo[x][ColumnaIni] = 1;
-            jugador.tablero.tamanyo[x][ColumnaIni - 1] = 2;
-            jugador.tablero.tamanyo[x][ColumnaIni + 1] = 2;
+            if(jugador.tablero.tamanyo[x][ColumnaIni]>=0){jugador.tablero.tamanyo[x][ColumnaIni] = 1;};
+            if(jugador.tablero.tamanyo[x][ColumnaIni - 1]>=0){jugador.tablero.tamanyo[x][ColumnaIni - 1] = 2;};
+            if(jugador.tablero.tamanyo[x][ColumnaIni + 1]>=0){jugador.tablero.tamanyo[x][ColumnaIni + 1] = 2;};
            }
     }
+    
 }
